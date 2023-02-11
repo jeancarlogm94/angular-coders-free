1e190ba16e41c926c7aa3a896c843c9cf104a24e
Jean Carlo Gonzalez Martinez <jg@dfvz.org>
2023-02-11 18:20:27 -0500

    class y events

--- curso_angular/src/app/botones/botones.component.ts
+++ curso_angular/src/app/botones/botones.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-botones',
   templateUrl: './botones.component.html',
-  styleUrls: ['./botones.component.css']
+  styleUrls: ['./botones.component.css'],
 })
 export class BotonesComponent {
+  textColor: string = '';
 
+  buttonDisabled: boolean = false;
 }
