a0865d32085371025a03b8faf45cec2744636205
Jean Carlo Gonzalez Martinez <jg@dfvz.org>
2023-02-11 17:56:39 -0500

    add botones

--- curso_angular/src/app/app.module.ts
+++ curso_angular/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { BotonesComponent } from './botones/botones.component';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent],
+  declarations: [AppComponent, ContadorComponent, BotonesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- curso_angular/src/app/contador/contador.component.ts
+++ curso_angular/src/app/contador/contador.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Person } from '../person';
 
 @Component({
   selector: 'app-contador',
   templateUrl: './contador.component.html',
-  styleUrls: ['./contador.component.css']
+  styleUrls: ['./contador.component.css'],
 })
 export class ContadorComponent {
+  person: Person = {
+    name: 'Jean Gonzalez',
+    age: 28,
+  };
 
+  number: number = 1;
+
+  decrement() {
+    this.number--;
+  }
+
+  increment() {
+    this.number++;
+  }
 }
