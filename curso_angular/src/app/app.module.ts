4eeed3a31eb8109c926f6d0167265b736c827c91
Jean Carlo Gonzalez Martinez <jg@dfvz.org>
2023-02-11 19:29:32 -0500

    images

--- curso_angular/src/app/app.module.ts
+++ curso_angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { BotonesComponent } from './botones/botones.component';
+import { ImagenesComponent } from './imagenes/imagenes.component';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, BotonesComponent],
+  declarations: [AppComponent, ContadorComponent, BotonesComponent, ImagenesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ curso_angular/src/app/imagenes/imagenes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagenes',
+  templateUrl: './imagenes.component.html',
+  styleUrls: ['./imagenes.component.css'],
+})
+export class ImagenesComponent {
+  src: string =
+    'https://images.pexels.com/photos/14800043/pexels-photo-14800043.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load';
+}
