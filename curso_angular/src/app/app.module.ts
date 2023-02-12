4f922a88ca1c47eea8d3f185bc3c6b49754a8d56
Jean Carlo Gonzalez Martinez <jg@dfvz.org>
2023-02-11 21:23:40 -0500

    formularios

--- curso_angular/src/app/app.module.ts
+++ curso_angular/src/app/app.module.ts
@@ -8,10 +8,22 @@ import { ImagenesComponent } from './imagenes/imagenes.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { BuclesComponent } from './bucles/bucles.component';
 import { SwitchComponent } from './switch/switch.component';
+import { Formulario2Component } from './formulario2/formulario2.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, BotonesComponent, ImagenesComponent, FormularioComponent, BuclesComponent, SwitchComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    BotonesComponent,
+    ImagenesComponent,
+    FormularioComponent,
+    BuclesComponent,
+    SwitchComponent,
+    Formulario2Component,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
